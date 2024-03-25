122ce897e6bd479c30e9038ca6e40111db31f6b4
jainoos007 <142651398+jainoos007@users.noreply.github.com>
2024-03-26 02:54:31 +0530

    todo-list.js

--- todo-list.js
+++ todo-list.js
@@ -45,23 +45,6 @@ function renderTodolist() {
     todolistHtml += html;
   });
 
-  /*for (let i = 0; i < todolist.length; i++) {
-    const todoobject = todolist[i];
-    // const name = todoobject.name;
-    // const date = todoobject.date;
-    const {name,date} = todoobject;
-    const html = `
-      <div>${name}</div>
-      <div>${date}</div>
-      <button onclick = "
-        todolist.splice(${i},1);
-        renderTodolist();
-      " class = "delete-button">Delete</button>
-    
-    `;
-    todolistHtml += html;
-
-  }*/
   document.querySelector('.js-elements')
     .innerHTML = todolistHtml;
 
